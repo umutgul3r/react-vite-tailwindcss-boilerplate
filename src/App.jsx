5b69d468db63c2d730202cf76b102df7f678af84
Umut <62966040+umutgul3r@users.noreply.github.com>
2021-12-24 01:37:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import "./App.css";
+
 
 function App() {
   return (
-    <div class="text-[155px] font-bold underline decoration-red-600 bg-orange-200  hover:bg-black ">
-      umut
+    <div class="text-[155px] font-bold underline decoration-red-600 bg-orange-200">
+      tailwindcss
     </div>
   );
 }
