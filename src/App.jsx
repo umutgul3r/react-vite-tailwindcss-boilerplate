c310a29cde7b6b965fac6d11fd7d4437daa733f2
umutgul3r <umutgul3r@gmail.com>
2021-12-24 01:33:41 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div class="text-[155px] font-bold underline decoration-red-600 bg-orange-200  hover:bg-black ">
+      umut
+    </div>
+  );
+}
+
+export default App;
